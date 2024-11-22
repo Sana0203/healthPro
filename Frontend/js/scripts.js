71e50936fad8cbf8be1eb80c10687b864d73b770
AmritjotSingh <amritjot.singh3@student.kpu.ca>
2024-11-22 15:52:19 -0800

    loginpage

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -1,14 +1,14 @@
 const testResults = [
     { id: 1, patientId: 'P001', name: "John Doe", testName: "Blood Test - Routine Hematology", date: "2024-01-15", result: "Normal" },
     { id: 2, patientId: 'P002', name: "Jane Smith", testName: "Liver Function Test", date: "2024-01-20", result: "Abnormal" },
-    { id: 3, patientId: 'P001', name: "John Doe", testName: "CT Scan", date: "2024-01-25", result: "Normal" },
-    { id: 4, patientId: 'P003', name: "Alice Johnson", testName: "Urine Test", date: "2024-02-05", result: "Normal" },
-    { id: 5, patientId: 'P002', name: "William Smith", testName: "X-Ray", date: "2024-02-10", result: "Abnormal" },
-    { id: 6, patientId: 'P001', name: "George Oliver", testName: "X-Ray", date: "2024-02-20", result: "Normal" },
-    { id: 7, patientId: 'P002', name: "Ellis Harry", testName: "Liver Function Test", date: "2024-10-10", result: "Normal" },
-    { id: 8, patientId: 'P001', name: "Jacob Ivy", testName: "CT Scan", date: "2024-01-25", result: "Normal" },
-    { id: 9, patientId: 'P003', name: "Edward Thomas", testName: "Urine Test", date: "2024-02-05", result: "Normal" },
-    { id: 10, patientId: 'P002', name: "Jane Smith", testName: "X-Ray", date: "2024-02-10", result: "Abnormal" },
+    { id: 3, patientId: 'P003', name: "Jack Kesler", testName: "CT Scan", date: "2024-01-25", result: "Normal" },
+    { id: 4, patientId: 'P004', name: "Alice Johnson", testName: "Urine Test", date: "2024-02-05", result: "Normal" },
+    { id: 5, patientId: 'P005', name: "William Smith", testName: "X-Ray", date: "2024-02-10", result: "Abnormal" },
+    { id: 6, patientId: 'P006', name: "George Oliver", testName: "X-Ray", date: "2024-02-20", result: "Normal" },
+    { id: 7, patientId: 'P007', name: "Ellis Harry", testName: "Liver Function Test", date: "2024-10-10", result: "Normal" },
+    { id: 8, patientId: 'P008', name: "Jacob Ivy", testName: "CT Scan", date: "2024-01-25", result: "Normal" },
+    { id: 9, patientId: 'P009', name: "Edward Thomas", testName: "Urine Test", date: "2024-02-05", result: "Normal" },
+    { id: 10, patientId:'P0010',name: "Chin Ken", testName: "X-Ray", date: "2024-02-10", result: "Abnormal" },
 
 ];
 
